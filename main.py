41db3616e658b63c9bb7b325f22a85b165a39169
AlexandruRaul <alexandrurak74@gmail.com>
2025-11-10 18:12:28 +0100

    domain update

--- main.py
+++ main.py
@@ -20,7 +20,9 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://portfolio-frontend-three-omega.vercel.app"
+    "https://portfolio-frontend-three-omega.vercel.app",
+    "https://alexandrurak.com",                 
+    "https://www.alexandrurak.com"
 ]
 app.add_middleware(
     CORSMiddleware,
